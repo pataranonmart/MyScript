21a0e8290f7e6fa78e85492064019665f7c47198
pataranonmart <52291297+pataranonmart@users.noreply.github.com>
2019-09-13 16:48:27 +0700

    Add files via upload

---
+++ assert.py
@@ -0,0 +1,9 @@
+chars	=	[	"Alpha"	,	"Beta"	,	"Gamma"	,	"Delta"	,	"Epsilon"	]
+def	display(	elem	)	: 
+	assert	type(	elem	)	is	int	,	"Argument	Must	Be	Integer!"												
+	print(	"List	Element"	,	elem	,	"="	,	chars[	elem	]	) 
+elem	=	4 
+display(	elem	)
+elem	=	4 
+display(	elem	) 
+
